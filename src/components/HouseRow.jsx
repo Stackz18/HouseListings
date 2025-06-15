3453d81ae71b52fa28a64db94be23e1507fc29fb
Anthony Stackman <Anth.stackman@hotmail.co.uk>
2025-06-15 07:18:22 +0100

    Initial commit

---
+++ src/components/HouseRow.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import currencyFormatter from "../helpers/currencyFormatter.js";
+
+const HouseRow = ({ house }) => {
+    return (
+        <tr>
+            <td>{house.address}</td>
+            <td>{house.country}</td>
+            <td>{currencyFormatter.format(house.price)}</td>
+        </tr>
+    );
+}
+
+const HouseRowMem = React.memo(HouseRow);
+
+export default HouseRow;
+export {HouseRowMem};
